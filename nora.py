0ee822fdffa5b0c07be65c32ce7d88b48d6f3572
drprojects <damien.robert@uzh.ch>
2024-05-23 09:45:34 +0200

    fix some errors when missing author or abstrat from elsevier. Removing the ungraceful try-except conditions in the main script

--- nora.py
+++ nora.py
@@ -21,43 +21,37 @@ def main(cfg):
     # Set up the proxy
     setup_proxy(cfg.proxy)
 
-    try:
-        # Load an arxiv paper
-        if cfg.arxiv.id or cfg.arxiv.title:
-            item = ArxivItem(arxiv_id=cfg.arxiv.id, title=cfg.arxiv.title)
+    # Load an arxiv paper
+    if cfg.arxiv.id or cfg.arxiv.title:
+        item = ArxivItem(arxiv_id=cfg.arxiv.id, title=cfg.arxiv.title)
 
-        # Load an Elsevier paper
-        elif cfg.elsevier.id or cfg.elsevier.doi:
-            item = ElsevierItem(cfg.elsevier.api_key, id=cfg.elsevier.id, doi=cfg.elsevier.doi)
+    # Load an Elsevier paper
+    elif cfg.elsevier.id or cfg.elsevier.doi:
+        item = ElsevierItem(cfg.elsevier.api_key, id=cfg.elsevier.id, doi=cfg.elsevier.doi)
 
-        # Load a Springer paper
-        elif cfg.springer.doi or cfg.springer.url:
-            item = SpringerItem(cfg.springer.api_key, doi=cfg.springer.doi, url=cfg.springer.url)
+    # Load a Springer paper
+    elif cfg.springer.doi or cfg.springer.url:
+        item = SpringerItem(cfg.springer.api_key, doi=cfg.springer.doi, url=cfg.springer.url)
 
-        # Load a HAL paper
-        elif cfg.hal.id or cfg.hal.title:
-            item = HALItem(hal_id=cfg.hal.id, title=cfg.hal.title)
+    # Load a HAL paper
+    elif cfg.hal.id or cfg.hal.title:
+        item = HALItem(hal_id=cfg.hal.id, title=cfg.hal.title)
 
-        # Load an entire Zotero library, may take a few seconds...
-        elif cfg.zotero.upload:
-            item = ZoteroLibrary(cfg.zotero, verbose=cfg.verbose)
+    # Load an entire Zotero library, may take a few seconds...
+    elif cfg.zotero.upload:
+        item = ZoteroLibrary(cfg.zotero, verbose=cfg.verbose)
 
-        # Fallback message
-        else:
-            print("Did not receive any argument requiring an upload to NORA")
-            item = None
+    # Fallback message
+    else:
+        print("Did not receive any argument requiring an upload to NORA")
+        item = None
 
-        # Upload data to NORA
-        if item is not None:
-            item.to_notion(cfg.notion, verbose=cfg.verbose)
+    # Upload data to NORA
+    if item is not None:
+        item.to_notion(cfg.notion, verbose=cfg.verbose)
 
-        # Dirty hack to remove the pesky folders
-        cleanup(root)
-
-    except:
-        # Dirty hack to remove the pesky folders
-        cleanup(root)
-
+    # Dirty hack to remove the pesky folders
+    cleanup(root)
 
 if __name__ == "__main__":
     main()
